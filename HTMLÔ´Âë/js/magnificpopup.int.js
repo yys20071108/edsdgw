1cacc2183ae443dce482aa41132da006403666ad
e装机 <88040111+yys20071108@users.noreply.github.com>
2023-08-12 20:54:33 +0800

    Delete HTMLÔ´Âë directory

--- HTMLÔ´Âë/js/magnificpopup.int.js
+++
@@ -1,9 +0,0 @@
-    //  VIDEO 
-$('.video-play-icon').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false
-});
