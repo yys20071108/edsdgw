b5e0ba1dfb3505cc7b22721d7b5e315bcaf338a4
Genouka <acutefox@foxmail.com>
2023-08-12 22:23:46 +0800

    删除加载动画

--- js/app.js
+++ js/app.js
@@ -38,9 +38,11 @@
 
 	// loader
 	$(window).on('load', function() {
+		/*
 	    $('#status').fadeOut();
 	    $('#preloader').delay(350).fadeOut('slow');
-	    $('body').delay(350).css({
+	    */
+	    $('body')/*.delay(350)*/.css({
 	        'overflow': 'visible'
 	    });
 	});
